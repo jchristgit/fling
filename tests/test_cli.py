20d6642528b03d65623851eed057374cb09ae743
Johannes Christ <jc@jchri.st>
2019-09-03 22:17:09 +0200

    Add `subTest` to CLI tests.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,15 +6,17 @@ from fling import cli
 
 class CLITests(unittest.TestCase):
     def test_parse_bind_address_for_valid_addresses(self):
-        self.assertEqual(
-            cli.parse_bind_address('127.0.0.1:1234'),
-            ('127.0.0.1', 1234)
+        cases = (
+            ('127.0.0.1:1234', ('127.0.0.1', 1234)),  # IPv4 loopback
+            ('::1:1234', ('::1', 1234))  # IPv6 loopback
         )
-        self.assertEqual(
-            # IPv6 loopback
-            cli.parse_bind_address('::1:1234'),
-            ('::1', 1234)
-        )
+
+        for (argument, expected) in cases:
+            with self.subTest(argument=argument, expected=expected):
+                self.assertEqual(
+                    cli.parse_bind_address(argument),
+                    expected
+                )
 
     def test_parse_bind_address_for_invalid_addresses(self):
         with self.assertRaises(ValueError):
